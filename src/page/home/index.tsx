45b51c36db18d72003b6669c153759e4a49d338b
mahdi <mahdisarrafz9@gmail.com>
2022-11-06 15:10:23 +0330

    test

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,21 +1,23 @@
-import { Button } from 'antd';
-import { useDispatch } from 'react-redux';
-import { logoutUser } from '../../slice/token';
-import './index.less';
+import { Button } from "antd";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../slice/token";
+import "./index.less";
 
+type Props = {};
 
-type Props = {}
+export default function Home({}: Props) {
+  const dispatch: any = useDispatch();
+  const handleLogout = () => {
+    dispatch(logoutUser({}));
+    window.location.reload();
+  };
 
-export default function Home({ }: Props) {
-    const dispatch: any = useDispatch()
-    const handleLogout = () => {
-        dispatch(logoutUser({}))
-        window.location.reload();
-    }
-
-    return (
-        <div className='main'>
-            <Button danger type='primary' onClick={() => handleLogout()}> LogOut</Button>
-        </div>
-    );
+  return (
+    <div className="main">
+      <Button danger type="secondary" onClick={() => handleLogout()}>
+        {" "}
+        LogOut
+      </Button>
+    </div>
+  );
 }
